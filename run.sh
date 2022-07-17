0da3ff29bc363fb7b0b8a385397c8e6b78a0ff36
kursadalsan <108494826+kursadalsan@users.noreply.github.com>
2022-07-17 04:23:26 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd ~/Desktop/
+mkdir scanfast
+cd ~/Desktop/scanfast/
+mkdir $1 
+cd $1
+amass enum -passive -d $1 > 1.txt
+httpx-toolkit -l 1.txt -o 3.txt
+nuclei -t -t cves/ -t exposures/ -l 3.txt
