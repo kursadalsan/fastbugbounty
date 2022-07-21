b0f225eeea2b78179b0aeb5f0ba359076dc7e890
kursadalsan <108494826+kursadalsan@users.noreply.github.com>
2022-07-21 10:19:49 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 mkdir $1 
 cd $1
 amass enum -passive -d $1 > 1.txt
-subfinder -d $1 -silent > 2.txt
+subfinder -d $1 -all > 2.txt
 cat *.txt > subs.txt
 httpx-toolkit -l subs.txt -o http-subs.txt
 nuclei -t cves/ -t exposures/ -l http-subs.txt
