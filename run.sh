6f6837fd6c5de3220f9b34365695324ab5a1cbf7
kursadalsan <108494826+kursadalsan@users.noreply.github.com>
2022-10-25 18:54:27 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 mkdir $1 
 cd $1
 amass enum -passive -d $1 > 1.txt
-subfinder -d $1 -all > 2.txt
+subfinder -d $1 > 2.txt
 cat *.txt > subs.txt
-httpx-toolkit -l subs.txt -o http-subs.txt
-nuclei -t cves/ -t exposures/ -l http-subs.txt
+httpx -l subs.txt -o http-subs.txt -t 150
+nuclei -t ~/Desktop/nuclei/log.yaml -t ~/Desktop/nuclei/log4j.yaml -t ~/Desktop/nuclei/spring.yaml -t ~/Desktop/nuclei/web.yaml -t cves/ -t default-logins/ -t exposed-panels/ -t takeovers/ -t vulnerabilities/ -t iot/ -l http-subs.txt -rl 250 -bs 50
