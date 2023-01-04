9db96b55b8b47176d6d2f297e0d0024330e22901
kursadalsan <108494826+kursadalsan@users.noreply.github.com>
2023-01-04 10:12:47 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ amass enum -passive -d $1 > 1.txt
 subfinder -d $1 > 2.txt
 cat *.txt > subs.txt
 httpx-toolkit -l subs.txt -o http-subs.txt -t 150
-nuclei -t cves/ -t default-logins/ -t exposed-panels/ -t takeovers/ -t vulnerabilities/ -t iot/ -l http-subs.txt -rl 250 -bs 50
+nuclei -rl 250 -bs 50 -s low -s medium -s high -s critical -l http-subs.txt
