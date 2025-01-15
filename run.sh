2febfcd129d24c4a19a7a579feb2f773dbf009da
kursadalsan <108494826+kursadalsan@users.noreply.github.com>
2025-01-15 12:34:02 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-mkdir $1 
+mkdir $1
 cd $1
-amass enum -passive -d $1 > 1.txt
-subfinder -d $1 > 2.txt
+subfinder -d $1 -all -recursive -t 200 -silent -o 1.txt
+assetfinder -subs-only $1 > 2.txt
+findomain -t $1 > 3.txt
+sublist3r -d $1 -t 50 -o 5.txt
+subdominator -d $1 > 6.txt
+
+
 cat *.txt > subs.txt
-httpx-toolkit -l subs.txt -o http-subs.txt -t 150
-nuclei -rl 250 -bs 50 -s low -s medium -s high -s critical -l http-subs.txt
+httpx-toolkit -l subs.txt -o http-subs.txt
+nuclei -l http-subs.txt -s critical,high,medium,low
